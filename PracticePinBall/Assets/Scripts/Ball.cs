d20089fa251fa5645ceb1c0b51a9903267a77db5
dylanvandenhemel <60535253+dylanvandenhemel@users.noreply.github.com>
2021-08-13 12:50:19 -0600

    BeforeCorrectPaddles

--- PracticePinBall/Assets/Scripts/Ball.cs
+++ PracticePinBall/Assets/Scripts/Ball.cs
@@ -6,25 +6,9 @@ public class Ball : MonoBehaviour
 {
     Vector3 ballStart;
 
-    private bool flickActive;
-    Vector3 flick;
-    PlayerControls pActions;
-
     private void Start()
     {
         ballStart = transform.position;
-        flick = new Vector3(0, 200, 0);
-    }
-
-    private void OnEnable()
-    {
-        pActions = new PlayerControls();
-        pActions.Enable();
-    }
-
-    private void OnDisable()
-    {
-        pActions.Disable();
     }
 
     private void OnTriggerStay(Collider other)
@@ -34,24 +18,6 @@ public class Ball : MonoBehaviour
             transform.position = ballStart;
         }
         
-        if(other.CompareTag("PaddleL"))
-        {
-            //Make Paddle work only on press not hold
-            if (pActions.PlayerActions.PaddleLeft.ReadValue<float>() != 0)
-            {
-                transform.GetComponent<Rigidbody>().AddForce(flick);
-            }
-
-        }
-        if (other.CompareTag("PaddleR"))
-        {
-            //Make Paddle work only on press not hold
-            if (pActions.PlayerActions.PaddleRight.ReadValue<float>() != 0)
-            {
-                transform.GetComponent<Rigidbody>().AddForce(flick);
-            }
-
-        }
     }
 
 
